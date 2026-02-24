46d64fb002948795d9ca1bcec159eb7dbcd00443
imgeekk <deadeyedseducer@gmail.com>
2026-02-24 21:54:53 +0530

    Made modal responsive. Additional features yet to be implemented

--- src/app/projects/[projectId]/components/TaskModal.tsx
+++ src/app/projects/[projectId]/components/TaskModal.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogPortal,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import RippleWaveLoader from "@/components/ui/ripple-loader";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { ComponentRef, useEffect, useRef, useState } from "react";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdDelete, MdKeyboardArrowDown } from "react-icons/md";
 import { CgDetailsMore } from "react-icons/cg";
 import useTaskModal from "@/hooks/use-task-modal";
 import { getTask } from "@/app/actions/services";
 import { useProject } from "@/lib/hooks/useProjects";
-import { Check } from "lucide-react";
+import { Check, Edit, TimerIcon, X } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/tooltip";
+import {
+  Popover,
+  PopoverAnchor,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button-1";
+import { WiTime4 } from "react-icons/wi";
+import { IoMdTime } from "react-icons/io";
+import { PiSquareHalfBottomFill } from "react-icons/pi";
 
 const TaskModal = () => {
   const taskId = useTaskModal((state) => state.taskId);
@@ -78,7 +95,7 @@ const TaskModal = () => {
   if (isLoading || !task) {
     return (
       <Dialog open={isOpen} onOpenChange={onClose}>
-        <DialogContent className="w-[95vw] max-w-[425px] mx-auto font-[inter] top-[10vh] !translate-y-0">
+        <DialogContent className="w-[95vw] max-w-[425px] mx-auto font-[inter] top-[10vh] !translate-y-0 p-0 h-15">
           <RippleWaveLoader className="h-5 w-1" />
         </DialogContent>
       </Dialog>
@@ -90,8 +107,8 @@ const TaskModal = () => {
 
     if (!trimmed) {
       setTitle(task!.title);
-      if(titleInputRef.current){
-        titleInputRef.current.textContent = title
+      if (titleInputRef.current) {
+        titleInputRef.current.textContent = title;
       }
       return;
     }
@@ -109,22 +126,36 @@ const TaskModal = () => {
   }
 
   function handleCancel() {
-    setTitle(task!.title)
+    setTitle(task!.title);
     setIsEditingTitle(false);
   }
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      
-      <DialogContent className="w-[95vw] max-w-[425px] max-h-[85vh] mx-auto font-[inter] top-[10vh] !translate-y-0 flex flex-col overflow-hidden">
-        <button className="text-black/90 font-[inter-bold] dark:text-white/75 bg-black/20 dark:bg-zinc-800 dark:hover:bg-zinc-700 hover:cursor-pointer w-fit p-1 px-1.5 rounded-sm text-sm">
-          {task?.column.title}{" "}
-          <MdKeyboardArrowDown className="inline-block text-xl" />
-        </button>
-        <hr />
-        <div id="scrollable-content" className="flex-1 overflow-y-auto p-1">
-          <header className="relative flex mb-7">
-            <div id="checkbox-container" className="w-10 flex items-start justify-center py-3">
+      <DialogContent showCloseButton={false} className="p-0 w-[95vw] max-w-[425px] max-h-[85vh] mx-auto font-[inter] top-[10vh] !translate-y-0 flex flex-col overflow-hidden">
+        <DialogHeader className="px-6 h-15 flex flex-row items-center justify-between border-b-1">
+          <button className="text-black/90 font-[inter-bold] dark:text-white/75 bg-black/20 dark:bg-zinc-800 dark:hover:bg-zinc-700 hover:cursor-pointer w-fit p-1 px-1.5 rounded-sm text-sm">
+            {task?.column.title}{" "}
+            <MdKeyboardArrowDown className="inline-block text-xl" />
+          </button>
+          <div>
+          <button><MdDelete/></button>
+          <DialogClose asChild>
+            <Button variant="ghost" size="icon">
+              <X className="h-4 w-4" />
+            </Button>
+          </DialogClose>
+          </div>
+        </DialogHeader>
+        <div
+          id="scrollable-content"
+          className="flex-1 overflow-y-auto px-3 sm:px-5 py-5"
+        >
+          <header id="task-title-section" className="relative flex mb-7">
+            <div
+              id="checkbox-container"
+              className="w-10 flex items-start justify-center py-2 sm:py-3"
+            >
               <Tooltip>
                 <TooltipTrigger className="">
                   <div
@@ -143,8 +174,8 @@ const TaskModal = () => {
                     )}
                   </div>
                 </TooltipTrigger>
-                <TooltipContent className="p-1 rounded-[4px]">
-                  <div className="text-centertext-[10px]">
+                <TooltipContent className="ml-3 p-1 rounded-[4px] max-sm:hidden">
+                  <div className="text-center text-[10px]">
                     {task.completed ? "Mark Incomplete" : "Mark Complete"}
                   </div>
                 </TooltipContent>
@@ -171,17 +202,39 @@ const TaskModal = () => {
                   handleCancel();
                 }
               }}
-              className="text-3xl font-[inter-bold] h-fit px-2 w-full block break-words whitespace-pre-wrap rounded-[3px] focus:outline-1 focus:outline-blue-500 leading-snug cursor-pointer focus:cursor-text"
+              className="text-2xl sm:text-3xl font-[inter-bold] h-fit px-2 w-full block break-words whitespace-pre-wrap rounded-[3px] focus:outline-1 focus:outline-blue-500 leading-snug cursor-pointer focus:cursor-text"
             >
               {title}
             </span>
           </header>
+          <section className="flex items-center justify-start flex-wrap pl-10 mb-10 gap-3">
+            <Popover>
+              <PopoverTrigger asChild>
+                <button className="border-1 border-black/10 dark:border-white/10 rounded-[4px] cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-800 flex items-center justify-center p-1.5 px-2 gap-1 max-sm:text-sm">
+                  <IoMdTime size={18} />
+                  Due Date
+                </button>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">this</PopoverContent>
+            </Popover>
+            <Popover>
+              <PopoverTrigger asChild>
+                <button className="border-1 border-black/10 dark:border-white/10 rounded-[4px] cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-800 flex items-center justify-center p-1.5 px-2 gap-1 max-sm:text-sm">
+                  <PiSquareHalfBottomFill size={18} />
+                  Change Cover
+                </button>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">this</PopoverContent>
+            </Popover>
+          </section>
           <section className="flex">
             <div className="w-10 flex justify-center dark:text-white/90 text-black/90">
               <CgDetailsMore size={23} />
             </div>
             <div className="flex-1">
-              <p className="px-1.5 mb-2 text-[16px] font-[inter-med] text-black/90 dark:text-white/90">Description</p>
+              <p className="px-1.5 mb-2 text-[16px] font-[inter-med] text-black/90 dark:text-white/90">
+                Description
+              </p>
               {!isEditingDesc && !task.description && (
                 <div
                   onClick={() => {
@@ -200,19 +253,25 @@ const TaskModal = () => {
                   contentEditable
                   suppressContentEditableWarning
                   onBlur={(e) => {
-                    const newDescription = e.currentTarget.textContent?.trim() || "";
+                    const newDescription =
+                      e.currentTarget.textContent?.trim() || "";
                     setIsEditingDesc(false);
-                    if(!newDescription) return;
-                    if(newDescription !== task.description){
-                      updateTask({taskId: taskId!, data: {description: newDescription}})
-                    } else{ return;}
-                 }}
+                    if (!newDescription) return;
+                    if (newDescription !== task.description) {
+                      updateTask({
+                        taskId: taskId!,
+                        data: { description: newDescription },
+                      });
+                    } else {
+                      return;
+                    }
+                  }}
                   onKeyDown={(e) => {
-                    if(e.key === "Enter"){
+                    if (e.key === "Enter") {
                       setIsEditingDesc(false);
                       e.currentTarget.blur();
                     }
-                    if(e.key === "Escape"){
+                    if (e.key === "Escape") {
                       setIsEditingDesc(false);
                     }
                   }}
