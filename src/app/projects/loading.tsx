8d0c94ae22e9e54698529252ec275b5232af2556
imgeekk <deadeyedseducer@gmail.com>
2026-02-25 22:21:15 +0530

    Added cover color to the task schema

--- src/app/projects/loading.tsx
+++ src/app/projects/loading.tsx
@@ -5,7 +5,7 @@ import RippleWaveLoader from "@/components/ui/ripple-loader";
 const Loading = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <RippleWaveLoader />‎ 
+      <RippleWaveLoader />
     </div>
   );
 };
