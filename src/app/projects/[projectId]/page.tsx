9322bb8fc9782e5a4ca26550f5b9e50e912bb11f
imgeekk <deadeyedseducer@gmail.com>
2026-03-02 11:57:25 +0530

    Implemented trello like task complete ui/ux

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -5,7 +5,7 @@ import { Card } from "@/components/ui/card";
 import { useProject } from "@/lib/hooks/useProjects";
 import { Check, MoreHorizontal, Plus, Timer, X } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import RippleWaveLoader from "@/components/ui/ripple-loader";
 import React, { useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
@@ -82,6 +82,8 @@ const SortableTask = React.memo(function SortableTask({
     opacity: isDragging ? 0.3 : 1,
   };
 
+  const [justCompleted, setJustCompleted] = useState(false);
+
   if (task.isOptimistic) {
     const MotionCard = motion.create(Card);
     return (
@@ -130,53 +132,94 @@ const SortableTask = React.memo(function SortableTask({
   return (
     <div ref={setNodeRef} style={style} className="mb-2.5">
       <Card
-        style={{
-          backgroundColor: task.coverColor ? task.coverColor : ""
-        }}
-        className={`flex rounded-[4px] mx-1 dark:bg-[#1F1F1F] bg-white hover:cursor-pointer shadow-xs border-none dark:border-white/5 shadow-gray-400 dark:shadow-black/80 `}
+        style={{ backgroundColor: task.coverColor ? task.coverColor : "" }}
+        className={`group/card relative flex rounded-[4px] mx-1 dark:bg-[#1F1F1F] bg-white hover:cursor-pointer shadow-xs dark:shadow-none border border-transparent dark:border-white/10 shadow-gray-400`}
       >
+        <div className="absolute h-full flex items-start justify-center">
+          <div
+            id="checkbox-container"
+            onClick={(e) => {
+              e.stopPropagation();
+              if (!task.completed) setJustCompleted(true);
+              setTimeout(() => setJustCompleted(false), 600);
+              onSetComplete({ taskId: task.id, completed: !task.completed });
+            }}
+            className="py-3 px-1.5"
+          >
+            <div id="burst-particle-container" className="relative">
+              <AnimatePresence>
+                {justCompleted && (
+                  <>
+                    {[...Array(8)].map((_, i) => (
+                      <motion.div
+                        key={i}
+                        className={`absolute w-1 h-1 rounded-full ${task.coverColor ? "bg-black/50" : "bg-blue-400"}`}
+                        style={{ top: "50%", left: "50%" }}
+                        initial={{ x: 0, y: 0, opacity: 1, scale: 1 }}
+                        animate={{
+                          x: Math.cos((i * Math.PI * 2) / 8) * 28,
+                          y: Math.sin((i * Math.PI * 2) / 8) * 28,
+                          opacity: 0,
+                          scale: 0,
+                        }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 1, ease: "easeOut" }}
+                      />
+                    ))}
+                  </>
+                )}
+              </AnimatePresence>
+
+              <div
+                className={`w-4 h-4 rounded-xs border border-black/50 dark:border-white/35 flex items-center justify-center transition-opacity duration-200
+            ${task.completed ? `opacity-100 border-none  ${task.coverColor ? "bg-black/50" : "bg-blue-500"} ` : "opacity-0 group-hover/card:opacity-100"}
+            ${task.coverColor ? "border-white/70 dark:border-white/70" : ""}
+          `}
+              >
+                {task.completed && (
+                  <Check
+                    size={13}
+                    className={`text-white dark:text-black ${task.coverColor ? "text-white dark:text-white" : ""}`}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Text content */}
         <div
           {...attributes}
           {...listeners}
           onClick={() => taskModal.onOpen(task.id)}
-          className="flex-1 flex-col"
+          className="flex-1 flex-col min-w-0 wrap-normal p-2"
         >
           <span
-            className={`flex-1 flex items-center min-w-0 max-sm:text-sm leading-snug break-words p-2 !cursor-pointer `}
+            className={`flex-1 flex items-center min-w-0 max-sm:text-sm overflow-x-hidden leading-snug break-words !cursor-pointer transition-all duration-200
+        ${task.coverColor ? "text-white" : ""}
+        ${!task.completed ? "group-hover/card:pl-5" : "pl-5"}
+      `}
           >
             {task.title}
           </span>
           {(task.description || task.dueDate) && (
-            <section className="flex items-center justify-start gap-2.5 flex-wrap px-3 pb-2 text-black/40 dark:text-white/50">
-              {task.description && <CgDetailsMore size={16} />}
+            <section className="flex items-center justify-start gap-2.5 flex-wrap mt-2 text-neutral-500 dark:text-neutral-400">
+              {task.description && (
+                <CgDetailsMore
+                  size={16}
+                  className={`${task.coverColor ? "text-white/80" : ""}`}
+                />
+              )}
               {task.dueDate && (
-                <span className="flex items-center justify-center gap-1 text-[13px]">
+                <span
+                  className={`flex items-center justify-center gap-1 text-[13px] ${task.coverColor ? "text-white/80" : ""}`}
+                >
                   <IoMdTime size={16} />
                   {getDueDateLabel(task.dueDate)}
                 </span>
               )}
             </section>
           )}
-        </div>
-        <div
-          id="checkbox-container"
-          onClick={(e) => {
-            e.stopPropagation();
-            onSetComplete({ taskId: task.id, completed: !task.completed });
-          }}
-          className="flex items-start justify-center"
-        >
-          <div className="group h-fit w-full p-3">
-            <div
-              className={`w-4 h-4 rounded-xs border-1 border-black/50 dark:border-white/35 group-hover:border-black/20 dark:group-hover:border-white/20 transition-colors duration-100 flex items-center justify-center ${
-                task.completed ? "bg-blue-500 border-none" : "bg-transparent"
-              }`}
-            >
-              {task.completed && (
-                <Check size={15} className="text-white dark:text-black" />
-              )}
-            </div>
-          </div>
         </div>
       </Card>
     </div>
@@ -187,23 +230,58 @@ const SortableTask = React.memo(function SortableTask({
 
 function TaskOverlay({ task }: { task: Task }) {
   return (
-    <div className="group mb-2.5 rotate-2 opacity-90">
-      <Card className="flex flex-col rounded-[4px] mx-1 dark:bg-[#1F1F1F] bg-white shadow-xl border-none">
-        <div className="flex">
-          <span className="flex-1 flex items-center min-w-0 max-sm:text-sm leading-snug break-words p-2">
-            {task.title}
-          </span>
-          <div className="flex p-3 items-start justify-center">
-            <div
-              className={`w-4 h-4 rounded-xs border-1 border-black/30 dark:border-white/30 flex items-center justify-center ${
-                task.completed ? "bg-blue-500 border-none" : "bg-transparent"
-              }`}
-            >
-              {task.completed && (
-                <Check size={15} className="text-white dark:text-black" />
-              )}
+    <div className="mb-2.5 rotate-3 opacity-90">
+      <Card
+        style={{ backgroundColor: task.coverColor ? task.coverColor : "" }}
+        className={`group/card relative flex rounded-[4px] mx-1 dark:bg-[#1F1F1F] bg-white hover:cursor-pointer shadow-xs dark:shadow-none border border-transparent dark:border-white/10 shadow-gray-400`}
+      >
+        <div className="absolute h-full flex items-start justify-center">
+          <div id="checkbox-container" className="py-3 px-1.5">
+            <div id="burst-particle-container" className="relative">
+              <div
+                className={`w-4 h-4 rounded-xs border border-black/50 dark:border-white/35 flex items-center justify-center transition-opacity duration-200
+            ${task.completed ? `opacity-100 border-none  ${task.coverColor ? "bg-black/50" : "bg-blue-500"} ` : "opacity-0 group-hover/card:opacity-100"}
+            ${task.coverColor ? "border-white/70 dark:border-white/70" : ""}
+          `}
+              >
+                {task.completed && (
+                  <Check
+                    size={13}
+                    className={`text-white dark:text-black ${task.coverColor ? "text-white dark:text-white" : ""}`}
+                  />
+                )}
+              </div>
             </div>
           </div>
+        </div>
+
+        {/* Text content */}
+        <div className="flex-1 flex-col min-w-0 wrap-normal p-2">
+          <span
+            className={`flex-1 flex items-center min-w-0 max-sm:text-sm overflow-x-hidden leading-snug break-words !cursor-pointer transition-all duration-200
+        ${task.coverColor ? "text-white" : ""}
+        ${!task.completed ? "group-hover/card:pl-5" : "pl-5"}
+      `}
+          >
+            {task.title}
+          </span>
+          {(task.description || task.dueDate) && (
+            <section className="flex items-center justify-start gap-2.5 flex-wrap mt-2 text-neutral-500 dark:text-neutral-400">
+              {task.description && (
+                <CgDetailsMore
+                  size={16}
+                  className={`${task.coverColor ? "text-white/80" : ""}`}
+                />
+              )}
+              {task.dueDate && (
+                <span
+                  className={`flex items-center justify-center gap-1 text-[13px] ${task.coverColor ? "text-white/80" : ""}`}
+                >
+                  <IoMdTime size={16} />
+                </span>
+              )}
+            </section>
+          )}
         </div>
       </Card>
     </div>
@@ -336,12 +414,14 @@ const DraggableColumn = React.memo(function DraggableColumn({
   const taskIds = column.tasks.map((t) => t.id);
 
   return (
-    <div ref={setNodeRef} style={style} className="mx-2 sm:flex-shrink-0 w-75">
-      <Card
-        {...attributes}
-        {...listeners}
-        className="h-fit p-2 rounded-[6px] bg-[#F7F5F6] dark:bg-[#161616] shadow-none dark:shadow-xs border-1 border-black/10 dark:border-white/5 shadow-gray-400 dark:shadow-black/80"
-      >
+    <div
+      {...attributes}
+      {...listeners}
+      ref={setNodeRef}
+      style={style}
+      className="mx-2 sm:flex-shrink-0 w-75"
+    >
+      <Card className="h-fit p-2 rounded-[6px] bg-[#F7F5F6] dark:bg-[#161616] shadow-none dark:shadow-xs border-1 border-black/10 dark:border-white/5 shadow-gray-400 dark:shadow-black/80">
         {/* Column header — drag handle */}
         <div className="flex items-center justify-between mb-2 cursor-pointer">
           <div className="p-1">
